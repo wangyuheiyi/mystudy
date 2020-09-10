75e5761992d9011a94dbb6a26a9357fa72ae6e20
wangyuheiyi <wangy@shanggame.com>
2020-09-10 10:59:18 +0800

    项目整合

---
+++ websocketresid/src/main/java/com/heiyigame/websocketresid/WebsocketresidApplication.java
@@ -0,0 +1,13 @@
+package com.heiyigame.websocketresid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketresidApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebsocketresidApplication.class, args);
+    }
+
+}
---
+++ websocketresid/src/test/java/com/heiyigame/websocketresid/WebsocketresidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heiyigame.websocketresid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketresidApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
