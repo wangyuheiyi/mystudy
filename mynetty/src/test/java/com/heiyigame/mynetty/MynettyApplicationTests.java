cc19d6977cb51d69cd9295c83f03b80a17d9c352
wangyuheiyi <wangy@shanggame.com>
2021-04-26 17:51:41 +0800

    添加项目

---
+++ mynetty/src/test/java/com/heiyigame/mynetty/MynettyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heiyigame.mynetty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MynettyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
